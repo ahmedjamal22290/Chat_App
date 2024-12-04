5ca4d3e0cc112076631eecb3890cbf901f2b31ce
Ahmed Jamal <ahmedjamal22290@gmail.com>
2024-12-04 13:10:33 +0200

    rt

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,19 +1,3 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF009687);
-
-const test1 = Color(0xF12B2B9B);
-const test1 = Color(0xF12B2B9B);
-const test1 = Color(0xF12B2B9B);
-const test1 = Color(0xF12B2B9B);
-const test1 = Color(0xF12B2B9B);
-const test1 = Color(0xF12B2B9B);
-const test1 = Color(0xF12B2B9B);
-const test1 = Color(0xF12B2B9B);
-const test1 = Color(0xF12B2B9B);
-const test2 = Color(0xE12EBE18);
-const test2 = Color(0xE12EBE18);
-const test2 = Color(0xE12EBE18);
-const test2 = Color(0xE12EBE18);
-const test2 = Color(0xE12EBE18);
-const test2 = Color(0xE12EBE18);
