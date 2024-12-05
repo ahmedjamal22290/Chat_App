8798a3bbf6208000aee7558ca6059d33c90f2102
Ahmed Jamal <ahmedjamal22290@gmail.com>
2024-12-05 15:05:12 +0200

    refactot message box widgets

--- lib/screens/chat_page.dart
+++ lib/screens/chat_page.dart
@@ -32,52 +32,83 @@ class chatPage extends StatelessWidget {
         padding: const EdgeInsets.only(top: 10.0),
         child: ListView(
           children: [
-            ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: 70,
-              ),
-              child: Container(
-                margin: EdgeInsets.only(right: 45, left: 2, bottom: 10),
-                padding: EdgeInsets.only(left: 6),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(12),
-                      bottomRight: Radius.circular(12),
-                      topLeft: Radius.circular(12)),
-                  color: KChatFirstColor,
-                ),
-                child: Center(
-                  child: Text(
-                    'ahmed jamal abd El-Zaher',
-                    style: TextStyle(fontSize: 25, color: Colors.white),
-                  ),
-                ),
-              ),
+            messageBoxUser1(
+              message: "Ahmed Jamal AbdEl-Zaher",
             ),
-            ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: 70,
-              ),
-              child: Container(
-                margin: EdgeInsets.only(left: 45, right: 2, bottom: 10),
-                padding: EdgeInsets.only(left: 6),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(12),
-                      bottomLeft: Radius.circular(12),
-                      topLeft: Radius.circular(12)),
-                  color: KChatSecondColor,
-                ),
-                child: Center(
-                  child: Text(
-                    textAlign: TextAlign.start,
-                    'ahmed jamal El-Rozany',
-                    style: TextStyle(fontSize: 25, color: Colors.white),
-                  ),
-                ),
-              ),
+            messageBoxUser2(
+              message: "Ahmed Jamal El-Rozany",
+            ),
+            messageBoxUser2(
+              message: "Ahmed Jamal El-Rozany",
+            ),
+            messageBoxUser1(
+              message: "Ahmed Jamal AbdEl-Zaher",
+            ),
+            messageBoxUser2(
+              message: "Ahmed Jamal El-Rozany",
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class messageBoxUser2 extends StatelessWidget {
+  messageBoxUser2({super.key, required this.message});
+  String message;
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        minHeight: 70,
+      ),
+      child: Container(
+        margin: EdgeInsets.only(left: 45, right: 2, bottom: 10),
+        padding: EdgeInsets.only(left: 6),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(12),
+              bottomLeft: Radius.circular(12),
+              topLeft: Radius.circular(12)),
+          color: KChatSecondColor,
+        ),
+        child: Center(
+          child: Text(
+            textAlign: TextAlign.start,
+            message,
+            style: TextStyle(fontSize: 25, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class messageBoxUser1 extends StatelessWidget {
+  messageBoxUser1({super.key, required this.message});
+  String message;
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        minHeight: 70,
+      ),
+      child: Container(
+        margin: EdgeInsets.only(right: 45, left: 2, bottom: 10),
+        padding: EdgeInsets.only(left: 6),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(12),
+              bottomRight: Radius.circular(12),
+              topLeft: Radius.circular(12)),
+          color: KChatFirstColor,
+        ),
+        child: Center(
+          child: Text(
+            message,
+            style: TextStyle(fontSize: 25, color: Colors.white),
+          ),
         ),
       ),
     );
