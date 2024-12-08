e307416366cf6aa70da0d25ec33a5c8251685270
Ahmed Jamal <ahmedjamal22290@gmail.com>
2024-12-08 11:29:06 +0200

    creating streamBuilder to get data from database

--- lib/screens/chat_page.dart
+++ lib/screens/chat_page.dart
@@ -4,11 +4,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:k/constants.dart';
 import 'package:k/widgets/message_box.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class chatPage extends StatelessWidget {
   static String id = 'chatPage';
   CollectionReference message =
       FirebaseFirestore.instance.collection(kMessagesCollection);
+
+  final Stream<QuerySnapshot> _messageStream =
+      FirebaseFirestore.instance.collection(kMessagesCollection).snapshots();
+
   TextEditingController _controller = TextEditingController();
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,15 +42,28 @@ class chatPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: 20,
-              itemBuilder: (context, index) {
-                return index % 2 == 0
-                    ? messageBoxUser1(message: 'message1')
-                    : messageBoxUser2(message: 'message2');
-              },
-            ),
+          StreamBuilder(
+            stream: _messageStream,
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError) {
+                log('error');
+                return Text('There was an error');
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                log('Wating');
+                return Text('data');
+              } else {
+                return ListView(
+                  children:
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> data =
+                        document.data()! as Map<String, dynamic>;
+                    return Text(data['message']);
+                  }).toList(),
+                );
+              }
+            },
           ),
           Padding(
             padding: const EdgeInsets.all(10.0),
